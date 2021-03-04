636b9551d7ce8962ba5df11af4b95db959356d45
Vy Nguyen <71942987+thevynguyen@users.noreply.github.com>
2021-03-04 01:19:19 -0500

    Working Modals

---
+++ main.js
@@ -0,0 +1,63 @@
+
+// White
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+} 
+
+//Pink
+function togglePopup2(){
+    document.getElementById("popup-2").classList.toggle("active");
+} 
+
+//Blue
+function togglePopup3(){
+    document.getElementById("popup-3").classList.toggle("active");
+} 
+
+//Brown
+function togglePopup4(){
+    document.getElementById("popup-4").classList.toggle("active");
+} 
+
+//Lime
+function togglePopup5(){
+    document.getElementById("popup-5").classList.toggle("active");
+} 
+
+//Black
+function togglePopup6(){
+    document.getElementById("popup-6").classList.toggle("active");
+} 
+
+//Yellow
+function togglePopup7(){
+    document.getElementById("popup-7").classList.toggle("active");
+} 
+
+//Green
+function togglePopup8(){
+    document.getElementById("popup-8").classList.toggle("active");
+} 
+
+//Purple
+function togglePopup9(){
+    document.getElementById("popup-9").classList.toggle("active");
+} 
+
+//Cyan
+function togglePopup10(){
+    document.getElementById("popup-10").classList.toggle("active");
+} 
+
+//Red
+function togglePopup11(){
+    document.getElementById("popup-11").classList.toggle("active");
+} 
+
+//Orange
+function togglePopup12(){
+    document.getElementById("popup-12").classList.toggle("active");
+} 
+
+
+
