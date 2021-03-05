a7bc016540bbf450046bd4d8db5d3c626e92a52e
Vy Nguyen <71942987+thevynguyen@users.noreply.github.com>
2021-03-05 00:25:47 -0500

    Redid entire modal
    Finally working!

--- main.js
+++ main.js
@@ -1,63 +1,65 @@
 
-// White
-function togglePopup(){
-    document.getElementById("popup-1").classList.toggle("active");
-} 
+//
 
-//Pink
-function togglePopup2(){
-    document.getElementById("popup-2").classList.toggle("active");
-} 
-
-//Blue
-function togglePopup3(){
-    document.getElementById("popup-3").classList.toggle("active");
-} 
-
-//Brown
-function togglePopup4(){
-    document.getElementById("popup-4").classList.toggle("active");
-} 
-
-//Lime
-function togglePopup5(){
-    document.getElementById("popup-5").classList.toggle("active");
-} 
-
-//Black
-function togglePopup6(){
-    document.getElementById("popup-6").classList.toggle("active");
-} 
-
-//Yellow
-function togglePopup7(){
-    document.getElementById("popup-7").classList.toggle("active");
-} 
-
-//Green
-function togglePopup8(){
-    document.getElementById("popup-8").classList.toggle("active");
-} 
-
-//Purple
-function togglePopup9(){
-    document.getElementById("popup-9").classList.toggle("active");
-} 
-
-//Cyan
-function togglePopup10(){
-    document.getElementById("popup-10").classList.toggle("active");
-} 
-
-//Red
-function togglePopup11(){
-    document.getElementById("popup-11").classList.toggle("active");
-} 
-
-//Orange
-function togglePopup12(){
-    document.getElementById("popup-12").classList.toggle("active");
-} 
+// function fade2Content() {
+//     console.log('fade2Content called');
+//     document.body.classList.remove("blackScreen");
+// }
 
 
+var whiteObj = document.getElementById('white');
 
+const displayObj = document.getElementById('display');
+
+
+whiteObj.addEventListener('click', function (evt) {
+    console.log('white button clicked: ' + evt.target.id);
+    const idName = evt.target.id;
+    userPicked(idName);
+});
+
+function userPicked(whichOne) {
+    console.log('userPicked called: ' + whichOne);
+    switch (whichOne) {
+        case 'white':
+            // displayObj.src = 'images/background.png';
+            zDisplayObj.src = 'images/red_card.svg';
+            // display title
+            zTitleObj.innerHTML = 'WHITE CREWMATE';
+            // display date range
+            zDateRangeObj.innerHTML = '##-Month to ##-Month';
+            // display description
+            zMoreInfo.innerHTML = '<p>Red tends to be the most intelligent one out of all the colors since this Crewmate has a long history of being the most suspicious contender of being an Imposter. There is a myth going around that Red might be the real imposter out of all of them.<p>';
+            // show hide modal
+            modal.hidden = !modal.hidden;
+            // play sound
+            break;
+        // case 'pink':
+        //     // displayObj. src = 'images
+        //     zDisplayObj.src = 'images/red_card.svg';
+        //     // display title
+        //     zTitleObj.innerHTML = 'PINK CREWMATE';
+        //     // display date range
+        //     zMoreInfo.innerHTML = '##-Month to ##-Month';
+        //     // display description 
+        //     zMoreInfo.innerHTML = '<p>Pink is not the imposter, dont kill.<p>';
+        //     // show hide modal
+        //     modal.hidden = !modal.hidden;
+        //     // play sound
+    }
+}
+
+// Modal Controls
+
+const modal = document.getElementById('modal');
+
+const closeModalObj = document.getElementById('btn-modal-close');
+closeModalObj.addEventListener('click', function () {
+    modal.hidden = !modal.hidden;
+});
+
+// // Link modal elements/objects to JS
+const zDisplayObj = document.getElementById('zDisplayImg');
+const zTitleObj = document.getElementById('zTitle');
+const zDateRangeObj = document.getElementById('zDateRange');
+const zMoreInfoObj = document.getElementById('zMoreInfo');
